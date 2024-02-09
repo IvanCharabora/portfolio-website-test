5517b9a511a49d5170f9dcbf924bc54a9e010d8a
Ivan Charabora <ivan@koibanx.com>
2024-02-09 17:59:31 -0300

    first custom commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,9 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Ivan Chara | Personal Portfolio",
+  // description: "Ricardo is a full-stack developer with 8 years of experience.",
+  description: "Charita",
 };
 
 export default function RootLayout({
